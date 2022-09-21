6af10a6678cec3e81aff8c51c050dbf3c5df4d1f
LifesMystery <arjunvsudn@gmail.com>
2022-09-21 18:15:49 -0500

    operator detection working

--- Fraction.java
+++ Fraction.java
@@ -5,32 +5,70 @@ public class Fraction
 {
     public static void main(String[] args)
     {
+        
         Scanner in = new Scanner(System.in);
         int[] numerator = new int[5];
         int[] denominator = new int[5];
+        System.out.print("Please input an expression in the order: num/denom (+/*-) num/denom --> ");
+        
         String input = in.nextLine();
         char[] chars = input.toCharArray();
         int operation = 0;
-        for(int i = 0; i<chars.length; i++)
+        int operatorIndex=999;
+        int firstSlash = input.indexOf("/");
+    
+        for(int i = firstSlash+1; i<chars.length; i++)
         {
-            switch(char[i])
-            {
-                case '+':
-                    operation = 0;
-                    break;
-                case '*':
-                    operation = 1;
-                case '/':
-                    operation = 2;
-                case '-':
-                    operation = 3;
-                default:
-                    System.out.println("Please input an expression in the order: num/denom +/*- num/denom; or input just a fraction in num/denom form");
+
+            
+            
+                switch(chars[i])
+                {
+                    case '+':
+                        operation = 1;
+                        operatorIndex=i;
+                        break;
+                    case '*':
+                        operation = 2;
+                        operatorIndex=i;
+                        break;
+                    case '/':
+                    
+                        operation = 3;
+                        operatorIndex=i;
+                        break;
+                    case '-':
+                        operation = 4;
+                        operatorIndex=i;
+                        break;
+                    default:
+                        if(i==chars.length-1 && operation==0)
+                        {
+                            System.out.println("ERROR: Please input an expression in the order: num/denom (+/*-) num/denom!");
+                            operation = 0;
+                            break;
+                        }
+                        break;
+                        
+                   
                     
 
 
-            }
+                }
+
+                if(operation!=0)
+                {
+                    System.out.println(operation + "" + operatorIndex);
+                    break;
+                }
+                
+            
+            
+
+            
         }
         
     }
+
+   
 }
